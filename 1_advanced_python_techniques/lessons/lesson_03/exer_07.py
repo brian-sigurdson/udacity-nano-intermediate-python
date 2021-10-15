81dad65794275bcd8396140caf874a97ad57c152
Brian Sigurdson <sigurdson.brian@gmail.com>
2021-10-15 17:01:30 -0400

    exer 7

--- 1_advanced_python_techniques/lessons/lesson_03/exer_07.py
+++ 1_advanced_python_techniques/lessons/lesson_03/exer_07.py
@@ -2,7 +2,6 @@
 
 
 def create_profile(given_name, *surnames, **details):
-
     print(given_name, *surnames)
     # print(given_name, " ".join(surnames))
     for key, value in details.items():
@@ -12,5 +11,21 @@ def create_profile(given_name, *surnames, **details):
 if __name__ == '__main__':
     create_profile("Sam")
     create_profile("Martin", "Luther", "King", "Jr.", born=1929, died=1968)
-    create_profile("Sebastian", "Thrun", cofounded="Udacity", experience="Stanford Professor")
+    create_profile("Sebastian", "Thrun", cofounded="Udacity",
+                   experience="Stanford Professor")
 
+# mylist = ["first", "second", "third", "forth"]
+# result = map(len, mylist)
+# # print(tuple(result))
+#
+# for elem in result:
+#     print(elem)
+
+
+# rolodex = ["amy", "brian"]
+# def is_member(name):
+#     return name in rolodex
+#
+# names = ["amy", "brian", "bob", "sue"]
+# result = filter(is_member, names)
+# print(list(result))
